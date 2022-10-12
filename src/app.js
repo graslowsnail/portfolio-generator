d6300fd97c4a3cdf204923740054bb19466f24f8
graslowsnail <graslowsnail@gmail.,com>
2022-10-12 18:22:15 -0500

    added input validation, also added ability to add multuple projects to projects array

--- src/app.js
+++ src/app.js
@@ -1,14 +1,136 @@
 const inquirer = require("inquirer");
 
-inquirer
-  .prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
       type: "input",
       name: "name",
-      message: "what is your name?"
-    }
-  ])
-    .then(answers => console.log(answers));
+      message: "what is your name? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("please ender gitHub username");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself",
+    },
+  ]);
+};
+
+const promptProject = (portfolioData) => {
+  console.log(`
+      =================
+      Add a New Project
+      =================
+    `);
+
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project? (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("please enter a project name");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a description of the project (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("please enter a discription of the project");
+            return false;
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "languages",
+        message: "What did you build this project with? (Check all that apply)",
+        choices: [
+          "JavaScript",
+          "HTML",
+          "CSS",
+          "ES6",
+          "jQuery",
+          "Bootstrap",
+          "Node",
+        ],
+      },
+      {
+        type: "input",
+        name: "link",
+        message: "Enter the GitHub link to your project. (Required)",
+        validate: (nameInput) => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log("please enter a link to the project");
+            return false;
+          }
+        },
+      },
+      {
+        type: "confirm",
+        name: "feature",
+        message: "Would you like to feature this project?",
+        default: false,
+      },
+      {
+        type: "confirm",
+        name: "confirmAddProject",
+        message: "Would you like to enter another project?",
+        default: false,
+      },
+    ])
+    .then((project) => {
+      portfolioData.projects.push(project);
+      if (project.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
+};
+
+promptUser()
+  .then((portfolioData) => promptProject(portfolioData))
+  .then((portfolioData) => {
+    console.log(portfolioData);
+  });
 
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
